47dbdeb4c748199022cd6d255fb5748624691fcc
VanshWebDev <vanshvanshkumar39@gmail.com>
2024-07-01 17:20:39 +0530

    chagned cors

--- server/socket/index.js
+++ server/socket/index.js
@@ -13,6 +13,7 @@ const server = http.createServer(app)
 const io = new Server(server,{
     cors : {
         origin : process.env.FRONTEND_URL,
+        methods : ['GET', 'POST'],
         credentials : true
     }
 })
