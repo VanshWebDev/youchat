4cf747e52f1f06e37e0b46f20740ead4e607ac2b
VanshWebDev <vanshvanshkumar39@gmail.com>
2024-07-01 20:09:15 +0530

    deploying

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : ["https://youchattt.netlify.app", "http://localhost:3000"],
+    origin : process.env.FRONTEND_URL,
     credentials : true
 }))
 app.use(express.json())
--- server/socket/index.js
+++ server/socket/index.js
@@ -13,7 +13,6 @@ const server = http.createServer(app)
 const io = new Server(server,{
     cors : {
         origin : process.env.FRONTEND_URL,
-        methods : ['GET', 'POST'],
         credentials : true
     }
 })
